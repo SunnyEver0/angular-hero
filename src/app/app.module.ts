d3738a283ca373d51f450e92a8d20893977447cf
rs16 <cdecmac02@cdecmac02deiMac.local>
2019-07-12 17:57:59 +0800

    update http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HttpClientModule }    from '@angular/common/http';
 import { InMemoryDataService }  from './in-memory-data.service';
 import { MessagesComponent } from './messages/messages.component';
 import { NgModule } from '@angular/core';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { NgModule } from '@angular/core';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,26 +1,26 @@
-import { Hero } from './hero';
-import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { Injectable } from '@angular/core';
+import { Hero } from "./hero";
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class InMemoryDataService implements InMemoryDbService {
-
   createDb() {
+    console.log('come here')
     const heroes = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: "Dr Nice" },
+      { id: 12, name: "Narco" },
+      { id: 13, name: "Bombasto" },
+      { id: 14, name: "Celeritas" },
+      { id: 15, name: "Magneta" },
+      { id: 16, name: "RubberMan" },
+      { id: 17, name: "Dynama" },
+      { id: 18, name: "Dr IQ" },
+      { id: 19, name: "Magma" },
+      { id: 20, name: "Tornado" }
     ];
-    return {heroes};
+    return { heroes };
   }
 
   // Overrides the genId method to ensure that a hero always has an id.
@@ -29,6 +29,9 @@ export class InMemoryDataService implements InMemoryDbService {
   // if the heroes array is not empty, the method below returns the highest
   // hero id + 1.
   genId(heroes: Hero[]): number {
-    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+    console.log(heroes, 'genId')
+    return heroes.length > 0
+      ? Math.max(...heroes.map(hero => hero.id)) + 1
+      : 11;
   }
 }
